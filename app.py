60544cb84eea6756781cbe6051a5e63dab2b71f1
HolyPi <joanelly2018@gmail.com>
2020-03-01 19:26:31 -0800

    test

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import os
 import requests
 import json
+import time
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,60 +14,66 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    url = "https://api.spoonacular.com/recipes/random"
+    # url = "https://api.spoonacular.com/recipes/random"
 
-    params = {
-        'number': 4,
-        'apiKey': apikey
-    }
+    # params = {
+    #     'number': 4,
+    #     'apiKey': apikey
+    # }
 
-    r = requests.get(url, params=params)
-    json_recipes = json.loads(r.content)
-    recipes = json_recipes['recipes']
+    # r = requests.get(url, params=params)
+    # json_recipes = json.loads(r.content)
+    # recipes = json_recipes['recipes']
 
-    return render_template('index.html', recipes=recipes)
+    # return render_template('index.html', recipes=recipes)
+    return render_template('index.html')
 
-@app.route('/recipe')
-def recipe():
+@app.route('/ajax/index')
+def ajax_index():
+    time.sleep(5)
+    return 'Done!'
 
-
-    search_term = request.args.get('user_input')
-
-
-    params = {
-        'query': search_term,
-        'apiKey': apikey,
-        'number': 6
-    }
-
-    url = "https://api.spoonacular.com/recipes/search"
+# @app.route('/recipe')
+# def recipe():
 
 
-    r = requests.get(url, params=params)
-    if r.status_code == 200:
-        json_recipes = json.loads(r.content)
-        recipes = json_recipes['results']
-        return render_template('recipe.html', recipes=recipes)
+#     search_term = request.args.get('user_input')
+
+
+#     params = {
+#         'query': search_term,
+#         'apiKey': apikey,
+#         'number': 6
+#     }
+
+#     url = "https://api.spoonacular.com/recipes/search"
+
+
+#     r = requests.get(url, params=params)
+#     if r.status_code == 200:
+#         json_recipes = json.loads(r.content)
+#         recipes = json_recipes['results']
+#         return render_template('recipe.html', recipes=recipes)
 
 
 
-@app.route('/recipe/<id>')
-def display_single_recipe(id):
-    params = {
-        'id': id,
-        "apiKey": apikey
-    }
-    url = f"https://api.spoonacular.com/recipes/{id}/information"
-    r = requests.get(url, params=params)
-    if r.status_code == 200:
-        json_recipe = json.loads(r.content)
-        ingredients = json_recipe['extendedIngredients']
-    else:
-        return "error"
-    return render_template('single_recipe.html', ingredients=ingredients)
+# @app.route('/recipe/<id>')
+# def display_single_recipe(id):
+#     params = {
+#         'id': id,
+#         "apiKey": apikey
+#     }
+#     url = f"https://api.spoonacular.com/recipes/{id}/information"
+#     r = requests.get(url, params=params)
+#     if r.status_code == 200:
+#         json_recipe = json.loads(r.content)
+#         ingredients = json_recipe['extendedIngredients']
+#     else:
+#         return "error"
+#     return render_template('single_recipe.html', ingredients=ingredients)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
     
     
